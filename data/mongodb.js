bea3c3c659bb586010c79ee6783c97d25f9a4fb3
Maartje Eyskens <meyskens@me.com>
2017-09-27 21:03:51 +0200

    fix space

--- data/mongodb.js
+++ data/mongodb.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose")
 mongoose.set("debug", false) // Having trouble? This option can help you!
-module.exports =mongoose.createConnection("mongodb://127.0.0.1/test") // Pass connection onwards
+module.exports = mongoose.createConnection("mongodb://127.0.0.1/test") // Pass connection onwards
