3b254697a1095c30206c2b6c924fb60412ec564d
Maartje Eyskens <meyskens@me.com>
2017-09-27 20:32:08 +0200

    use new method

--- data/mongodb.js
+++ data/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
 mongoose.set("debug", false)
-mongoose.connect("mongodb://127.0.0.1/test")
+mongoose.createConnection("mongodb://127.0.0.1/test")
 
 module.exports = mongoose
